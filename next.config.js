331a0e2c79b09644f41110cb3ec51fb4e7253502
rightson <chari.rightson@gmail.com>
2023-03-15 01:39:37 +0300

    update 3 files and create 2 files

--- next.config.js
+++ next.config.js
@@ -18,6 +18,8 @@ const nextConfig = withPWA({
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    CONTENTFUL_PREVIEW_ACCESS_TOKEN:
+      process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
   },
 });
 module.exports = nextConfig;
